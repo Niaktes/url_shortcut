d9a85e1a157ff735f15e03f025588aaef81a295f
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-10-29 18:29:30 +0300

    Entry point added.

---
+++ src/main/java/ru/job4j/shortcut/UrlShortcutApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.shortcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortcutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortcutApplication.class, args);
+	}
+
+}
